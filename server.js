e47ad3af518beb706a2b6e34bc5bb43cffd35ce8
jeffersondrs <jeffersonduarte@id.uff.br>
2022-09-07 23:49:21 -0300

    updating

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 
-dotenv.config({ path: './.env' });
+dotenv.config({ path: '.env' });
 
 const app = require('./app');
 const DB = process.env.DATABASE.replace(
